6fd770f9747901c1b652c4c30fcebf628748ab35
wakeup1pa <166738592+wakeup1pa@users.noreply.github.com>
2024-05-03 15:15:52 +0500

    keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,7 +1,7 @@
 from aiogram.types import WebAppInfo
 from aiogram import types
 
-web_app = WebAppInfo(url='site_url')
+web_app = WebAppInfo(url="https://wakeup1pa.github.io/bessonica.github.io/")
 
 keyboard = types.ReplyKeyboardMarkup(
     keyboard=[
