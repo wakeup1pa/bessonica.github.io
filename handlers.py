6c01be392a0e3db0cf83200f94abc7f67112b800
wakeup1pa <166738592+wakeup1pa@users.noreply.github.com>
2024-05-11 20:31:57 +0500

    handlers.py

--- handlers.py
+++ handlers.py
@@ -3,7 +3,7 @@ from keyboards import keyboard
 from aiogram import types
 from aiogram.dispatcher.filters import Command
 
-@dp.message_handler(Command('start'))
+@dp.message_handler(Command('/start'))
 async def start(message: types.Message):
     await bot.send_message(message.chat.id, 'Ожидаем  ваш заказ!',
                            reply_markup=keyboard)
