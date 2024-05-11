5393b7643439ff200737e252331505a5a8510209
wakeup1pa <166738592+wakeup1pa@users.noreply.github.com>
2024-05-11 19:01:16 +0500

    handlers.py

--- handlers.py
+++ handlers.py
@@ -22,7 +22,7 @@ async def buy_process(web_app_message):
     await bot.send_invoice(web_app_message.chat.id,
                            title='Laptop',
                            description='Description',
-                           provider_token='PAYMENTS_TOKEN',
+                           provider_token='pay_token',
                            currency='rub',
                            need_email=True,
                            prices=PRICE[f'{web_app_message.web_app_data.data}'],
