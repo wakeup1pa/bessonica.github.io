a14ed46f216d437321d648debaeecae01a17eed0
wakeup1pa <166738592+wakeup1pa@users.noreply.github.com>
2024-05-11 19:20:14 +0500

    keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -3,9 +3,9 @@ from aiogram import types
 
 web_app = WebAppInfo(url="https://wakeup1pa.github.io/bessonica.github.io/")
 
-keyboard = types.ReplyKeyboardMarkup(
+keyboard = ReplyKeyboardMarkup(
     keyboard=[
-        [types.KeyboardButton(text='Site', web_app=web_app)]
+        [KeyboardButton(text='Site', web_app=web_app)]
     ],
     resize_keyboard=True
 )
